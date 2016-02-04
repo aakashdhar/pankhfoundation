5fc3e2e852cc61c5f4dd7bb2acce5f5823c5fb92
Aakash Dhar <aakashdhar@ymail.com>
2016-02-05 03:46:49 +0530

    third commit

--- js/pankh.js
+++ js/pankh.js
@@ -1,15 +1,39 @@
-var map = new GMaps({
-	  div: '.map',
-	  lat: 28.4447562,
-	  lng: 77.1425387,
-	  zoom: 12
-	});
 
-	map.addMarker({
-  	lat: 28.4447562,
-  	lng: 77.1425387,
-  	title: 'Pankh Foundation',
-  	infoWindow: {
-  	content: '<p>Pankh foundation office</p>'
-	}
+$(document).ready(function() {
+	var map = new GMaps({
+		  div: '.map',
+		  lat: 28.4447562,
+		  lng: 77.1425387,
+		  zoom: 12
+		});
+
+		map.addMarker({
+	  	lat: 28.4447562,
+	  	lng: 77.1425387,
+	  	title: 'Pankh Foundation',
+	  	infoWindow: {
+	  	content: '<p>Pankh foundation office</p>'
+		}
+	});
+	$('a[href*=#]').bind("click", function(e){
+           
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+			e.preventDefault();
+	});
+    $(window).scroll(function() {
+        /* freeze menu once it hit 70px */
+        var scrollx = $(window).scrollTop();
+        $("section").each(function(){
+            var sectionx = $(this).offset().top - scrollx;
+            if(sectionx <=70){ // 70 for header
+             $(this).find("nav").addClass("freeze");
+            } else {
+             $(this).find("nav").removeClass("freeze");  
+            }
+        });
+    });
+    $(window).scrollTop($(window).scrollTop() + 1);
 });
